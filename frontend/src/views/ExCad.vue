388d4c17f2ee96ad5dba0906c7b5316b7e2db93a
Eddy Cizeron <eddy.cizeron@gmail.com>
2020-10-04 15:16:40 +0200

    Improve ExCad component

--- frontend/src/views/ExCad.vue
+++ frontend/src/views/ExCad.vue
@@ -1,33 +1,45 @@
 <template>
-  <table class="ex_cad_frame">
-    <tr v-for="row in getRows()" :key="row.index" :style="row.style">
-      <th style="font-style: italic">{{ row.token.authorName }}: </th>
-      <th>
-        <template v-if="!isSecret">
-          <span>{{ row.token.beginning }}&nbsp;{{ row.token.ending }}</span>
-        </template>
-        <template v-else-if="row.isLast">
-          <span :style="row.fuzzyStyle">{{ row.token.beginning }}</span>&nbsp;
-          <span>{{ row.token.ending }}</span>
-        </template>
-        <template v-else>
-          <span :style="row.fuzzyStyle">{{ row.token.beginning }}&nbsp;{{ row.token.ending }}</span>
-        </template>
-      </th>
-    </tr>
-  </table>
+  <div class="ex_cad_frame">
+    <table class="ex_cad_table">
+      <colgroup>
+       <col span="1" style="width:100px;">
+       <col span="1">
+      </colgroup>
+      <tr v-for="row in getRows()" :key="row.index" :style="row.style">
+        <th style="font-style: italic">{{ row.token.authorName }}: </th>
+        <th>
+          <template v-if="!isSecret">
+            <span>{{ row.token.beginning }}&nbsp;{{ row.token.ending }}</span>
+          </template>
+          <template v-else-if="row.isLast">
+            <span :style="row.fuzzyStyle">{{ row.token.beginning }}</span>&nbsp;
+            <span>{{ row.token.ending }}</span>
+          </template>
+          <template v-else>
+            <span :style="row.fuzzyStyle">{{ row.token.beginning }}&nbsp;{{ row.token.ending }}</span>
+          </template>
+        </th>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style>
 .ex_cad_frame {
-  width: 700px;
+  width: 100%;
+  height: 400px;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
+  background-size: 100%;
+  background-image: url("https://static.boredpanda.com/blog/wp-content/uploads/2015/03/Teletubbies-in-Black-White-Look-Like-A-Horror-Show__700.jpg");
+}
+.ex_cad_table {
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   font-weight: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  border-style: solid;
-  border-color: black;
-  border-width: 1px;
   align-content: left;
   text-align: left;
 }
@@ -35,7 +47,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-//import Chroma from 'chroma-js'
+import Chroma, { Color } from 'chroma-js'
 
 export class ExCadToken {
   beg$: string
@@ -52,40 +64,34 @@ export class ExCadToken {
 }
 
 type ColorStyle = {
-  backgroundColor: string;
-  color: string;
+  readonly backgroundColor: string;
+  readonly color: string;
+  readonly textShadow: string;
 }
 
 type TextColorStyle = {
-  color: string;
+  readonly color: string;
 }
 
 type ColorSet = {
-  textColor: string;
-  plainColor: string;
-  softenedColor: string;
+  readonly textColor: Chroma.Color;
+  readonly shadowColor: Chroma.Color;
+  readonly plainColor: Chroma.Color;
+  readonly softenedColor: Chroma.Color;
 }
 
 type ExCadRow = {
-  index: number;
-  isLast: boolean;
-  token: ExCadToken;
-  style: ColorStyle;
-  fuzzyStyle: TextColorStyle;
+  readonly index: number;
+  readonly isLast: boolean;
+  readonly token: ExCadToken;
+  readonly style: ColorStyle;
+  readonly fuzzyStyle: TextColorStyle;
 }
 
-function generateNiceColor(isDark: boolean): [number, number, number] {
-  const color: [number, number, number] = [0, 0, 0]
-  const offset = isDark ? 64 : 0
-  const j = Math.floor(3 * Math.random())
-  for (let i = 0; i < 3; i++)
-  {
-    if (i == j)
-      color[i] = isDark ? 0 : 255
-    else
-      color[i] = Math.floor(192 * Math.random()) + offset
-  }
-  return color
+function generateNiceColor(): Chroma.Color {
+  const hue = 360 * Math.random()
+  const lightness = 0.6 * Math.random() + 0.2
+  return Chroma.hsl(hue, 1.0, lightness)
 }
 
 const charCodeForA: number = 'a'.charCodeAt(0)
@@ -101,21 +107,33 @@ function generateText(): string {
   return text
 }
 
-const generatedSets: ColorSet[] =
-  Array(1000).fill(null).map(() => {
-    const isBgDark = Math.random() > 0.5
-    const bcolor = generateNiceColor(isBgDark)
-    const tcolor = isBgDark ? "white" : "black"
-    const scolor: [number, number, number] = [0, 0, 0]
-    scolor[0] = Math.floor(bcolor[0] * 0.75)
-    scolor[1] = Math.floor(bcolor[1] * 0.75)
-    scolor[2] = Math.floor(bcolor[2] * 0.75)
-    return {
-      plainColor: `rgb(${bcolor[0]}, ${bcolor[1]}, ${bcolor[2]})`,
-      textColor: tcolor,
-      softenedColor: `rgb(${scolor[0]}, ${scolor[1]}, ${scolor[2]})`,
+function generateColorSets(count: number): ColorSet[] {
+  const colorSets = Array(count).fill(null) as ColorSet[]
+  let i = 0;
+  while (i < count) {
+    const plainColor = generateNiceColor().alpha(0.8)
+    let tooSimilar = false
+    for (let j = 1; j < 5 && !tooSimilar; ++j) {
+      if (i - j < 0)
+        continue;
+      const previousColor = colorSets[i - j].plainColor
+      tooSimilar = Chroma.distance(previousColor, plainColor) < 30
     }
-  })
+    if (tooSimilar) {
+      console.log(`Collision for ${i}`)
+      continue
+    }
+    const plainColorIsLight = plainColor.lch()[0] > 75.0
+    const textColor = plainColorIsLight ? Chroma('black') : Chroma('white')
+    const shadowColor = plainColorIsLight ? plainColor.brighten(1.5) : plainColor.darken(2.5)
+    const softenedColor = plainColor.darken()
+    colorSets[i] = { plainColor, textColor, shadowColor, softenedColor, }
+    ++i
+  }
+  return colorSets
+}
+
+const generatedSets: ColorSet[] = generateColorSets(100)
 
 export default Vue.component('ex-cad',
 {
@@ -144,12 +162,15 @@ export default Vue.component('ex-cad',
       const length: number = this.tokens.length
       for (const otoken of this.tokens) {
         const set = generatedSets[index]
+        const shadowColor = set.shadowColor.hex('rgb')
         const style = {
-          backgroundColor: set.plainColor,
-          color: set.textColor,
+          backgroundColor: set.plainColor.hex('rgba'),
+          color: set.textColor.hex('rgb'),
+          textShadow: `-1px 0 ${shadowColor}, 0 1px ${shadowColor}, 1px 0 ${shadowColor}, 0 -1px ${shadowColor}`,
         }
         const fuzzyStyle = {
-          color: set.softenedColor,
+          color: set.softenedColor.hex('rgba'),
+          textShadow: "none",
         }
         const isLast = index == length - 1
         const token = this.transformToken(otoken, isLast)
--- frontend/src/views/FirebaseExample2.vue
+++ frontend/src/views/FirebaseExample2.vue
@@ -17,9 +17,13 @@ const FirebaseModule2 = Vue.extend({
       buttonText: "Show", 
       tokens: [
         new ExCadToken("Eddy", "C'est joli", "tout ça"),
-        new ExCadToken("Vivianne", "mais ça ne", "vaut pas"),
+        new ExCadToken("Viviannne", "mais ça ne", "vaut pas"),
         new ExCadToken("Aurélien", "la plupart", "des applications"),
         new ExCadToken("Kiki", "qu'on trouve", "dans le commerce"),
+        new ExCadToken("Lapin", "même si", "beaucoup de gens"),
+        new ExCadToken("Rose", "pense que", "les amanites"),
+        new ExCadToken("George", "phalloïdes", "poussent dans"),
+        new ExCadToken("Fabrice", "de vaste prairies", "où les chevaux"),
       ],
     }
   },
