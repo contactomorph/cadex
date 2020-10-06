43eeb675b109404dd3466f1d0985918e1f6415d1
Aurelien Moreau <aurelien.moreau@angus.ai>
2020-10-06 10:56:36 +0200

    Add forgotten file Stories

---
+++ frontend/src/views/Stories.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Créer un nouvelle histoire</h1>
+    <div>
+      <input v-model="players" min="2" type="number">
+      <button @click="newStory">Créer</button>
+      <div>
+        <a :href="storyUrl">Lien vers l'histoire</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Story } from '../backend'
+
+const StoryModule = Vue.extend({
+  data() {
+    return {
+      players: 2,
+      storyUrl: "",
+    }
+  },
+  methods: {
+    newStory () {
+      const story = new Story()
+      story.startRegistration(this.players).then(() => {
+        this.storyUrl = story.url
+      })
+    }
+  }
+})
+
+
+
+export default StoryModule
+</script>
