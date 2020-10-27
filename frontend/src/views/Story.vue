e09e12c48d05a85982177e42a76f09cbfba1d22d
Eddy Cizeron <eddy.cizeron@gmail.com>
2020-10-27 21:50:56 +0100

    Adapt story

--- frontend/src/views/Story.vue
+++ frontend/src/views/Story.vue
@@ -1,34 +1,34 @@
 <template>
 <div>
   <h1>L'histoire</h1>
-  <span v-if="story && story.data.registering">Histoire ouverte, vous pouvez vous inscrire</span>
-  <span v-else><span v-if="story && !story.data.completed">Histoire fermée, vous pouvez jouer</span></span>
-  <div v-if="story.data.completed">
-    <h3>C'est terminé</h3>
-    <div>
-      <span v-for="(player, num) in story.data.players" :key="num"><span v-if="player">{{player.head}}&nbsp;{{player.tail}}&nbsp;</span></span>
-    </div>
-  </div>
+  <p v-if="!story">Pas d'histoire</p>
+  <template v-else-if="story.data.registering">
+    <p>Histoire ouverte, vous pouvez vous inscrire</p>
+    <input type="text" v-model="name" placeholder="ton nom">
+    <button @click="join">Rejoindre / Changer de nom</button>
+  </template>
+  <p v-else-if="!story.data.completed">Histoire fermée, vous pouvez jouer</p>
+  <p v-else>Histoire terminée</p>
 
   <h1>Les joueurs</h1>
-  <div>
-    <div v-for="(player, num) in story.data.players" :key="num">
-      <span v-if="player">{{ player.name }}</span>
-    </div>
 
-    <input type="text" v-model="name" placeholder="ton nom"><button @click="join">Rejoindre / Changer de nom</button>
-  </div>
+  <ul v-for="(p, num) in story.data.players" :key="num">
+    <li v-if="p">
+      Joueur #{{num}}: {{ p.name }}
+      <span v-if="player && player.data.num === p.num"> (Moi)</span>
+    </li>
+  </ul>
 
-  <h1>Le joueur</h1>
-  Mon nom: <span v-if="player">{{ player.data.name }}</span>, <span v-if="player && player.data.myTurn">c'est mon tour, je dois continuer
-    "{{ player.data.ptail }}"
-  </span><span v-else>ce n'est pas mon tour</span>
-  <div>
-    <textarea type="text" v-model="head" placeholder="la suite"></textarea>
-    <textarea type="text" v-model="tail" placeholder="la fin"></textarea>
-    <button @click="play">Play</button>
-  </div>
+  <p v-if="player && !player.exists">Je n'ai pas encore de nom</p>
 
+  <h1>Le jeu</h1>
+  
+  <template v-if="story && player">
+    <ex-cad :tokens="tokens"></ex-cad>
+    <p v-if="story.data.completed">C'est terminé</p>
+    <p v-else-if="player.data.myTurn">A moi de jouer ! <button @click="play">Play</button></p>
+    <p v-else>Ce n'est pas mon tour</p>
+  </template>
 
   <div>
     <h1>Debug</h1>
@@ -41,8 +41,9 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from 'vue'
 import { Story, registerPlayer, Player, getUID } from '../backend'
+import { ExCadMode, ExCadToken } from "../components/ExCad.vue"
 
 const StoryModule = Vue.extend({
   data() {
@@ -50,12 +51,9 @@ const StoryModule = Vue.extend({
       story: null as null | Story,
       player: null as null | Player,
       name: '',
-      head: '',
-      tail: '',
       myTurn: false,
-      truc: null,
       text: '',
-      baduid: ''
+      baduid: '',
     }
   },
   created () {
@@ -68,22 +66,66 @@ const StoryModule = Vue.extend({
       player.enableAutoUpdate()
     })
   },
-  methods: {
-    async play() {
-      if (!this.player) {
-        return
+  computed: {
+    tokens: function(): ExCadToken[] {
+      if (!this.story || !this.player)
+        return []
+      const story: Story = this.story
+      if (story.data.registering)
+        return []
+      const player: Player = this.player
+      const tokens = [] as ExCadToken[];
+      const currentPlayerIndex = story.data.currentPlayer
+      const completed = story.data.completed
+      const isMyTurn: boolean = player.data.myTurn
+      const myNum: number = player.data.num
+      for(let i = 1; i <= currentPlayerIndex; ++i) {
+        const p = story.data.players[i]
+        if (p) {
+          const isMine = myNum === p.num
+          const isAlmostLast = p.num == currentPlayerIndex - 1
+          const isLast = p.num == currentPlayerIndex
+          const mode: ExCadMode =
+            completed || (!isMyTurn && isMine) ? ExCadMode.Disclosed :
+            !isMyTurn ? ExCadMode.Hidden :
+            isAlmostLast ? ExCadMode.HalfHidden :
+            isLast && isMine ? ExCadMode.ReadyForInput :
+            ExCadMode.Hidden
+          const name: string = p.name ?? "?"
+          const head: string = mode === ExCadMode.Disclosed && !completed ?
+            player.data.head :
+            p.head ?? ""
+          const tail: string = mode === ExCadMode.Disclosed && !completed ?
+            player.data.tail :
+            mode === ExCadMode.HalfHidden ?
+            player.data.ptail :
+            p.tail ?? ""
+          const token = new ExCadToken(name, head, tail, mode)
+          tokens.push(token)
+        }
       }
+      return tokens
+    }
+  },
+  methods: {
+    async play(): Promise<void> {
+      if (!this.player) {
+        return;
+      }
+      
+      const myToken = this.tokens[this.tokens.length - 1]
+
       await this.player.update({
         played: true,
-        head: this.head,
-        tail: this.tail
+        head: myToken.beginning,
+        tail: myToken.ending
       })
     },
-    async join () {
+    async join (): Promise<void> {
       if (!this.story || !this.player) {
         return
       }
-      const player = this.player
+      const player: Player = this.player
 
       await player.load()
 
