7d25561886efc660aaf35073ea5c02614b387f05
Aurelien Moreau <aurelien.moreau@yienyien.net>
2020-12-02 17:25:50 +0100

    Clean useless files and update menu

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
-import Firebase from '../views/FirebaseExample.vue'
 import Stories from '../views/Stories.vue'
 import Story from '../views/Story.vue'
 import Demo from '../views/Demo.vue'
@@ -10,31 +8,22 @@ Vue.use(VueRouter)
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/firebase',
-    name: 'Firebase',
-    component: Firebase
+    redirect: {name: 'Home'}
   },
   {
     path: '/stories',
-    name: 'Stories',
+    name: 'Home',
     component: Stories
   },
   {
     path: '/stories/:storyId',
     name: 'Story',
     component: Story
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/demo',
